26e6efa1371515ddf3d25f95fdf882e3fa06864c
rokisto2 <gaponenkooleg0@gmail.com>
2022-09-23 10:49:12 +0300

    Add comeent

--- Wikidb.py
+++ Wikidb.py
@@ -5,13 +5,13 @@ import pymysql
 
 
 class wikiDB:
-
+    #Подключение к бд
     def __init__(self):
         self.connect = pymysql.connect(host='localhost', user='root', password='1111', database="wiki")
 
     def __del__(self):
         self.connect.close()
-
+    #Добавление ссылок на обработку
     def add_raw_urls(self, urls):
         try:
             with self.connect.cursor() as cursor:
@@ -28,7 +28,7 @@ class wikiDB:
             self.connect.close()
         except:
             print('error 1')
-
+    #Добавление ссылок в главную таблицу
     def add_url(self, url: str, u):
         try:
 
@@ -41,7 +41,7 @@ class wikiDB:
 
         except Exception as e:
             return -1
-
+    #Поиск определённого url
     def find_url(self, url):
 
         with self.connect.cursor() as cursor:
@@ -51,7 +51,7 @@ class wikiDB:
             cursor.close()
             return row[0][0]
 
-
+    #Создание зависемостей от одной ссылки
     def creat_mig(self, id, url):
         try:
             with self.connect.cursor() as cursor:
@@ -62,8 +62,10 @@ class wikiDB:
         except:
             print('error')
 
+    #Получаем все термины которые в стречаються на данном сайте
     def get_word_from_url(self, url):
         with self.connect.cursor() as cursor:
+            #Берём все зависимые ссылки
             cursor.execute("SELECT wikiid FROM wikiaddiction WHERE url = %s", url)
             self.connect.commit()
             id = cursor.fetchall()
@@ -73,11 +75,13 @@ class wikiDB:
             ans = {}
             k = 1
             try:
+                #Берём все термины из ссылок
                 cursor.execute(f"SELECT termin FROM wikiurl where id IN ({ids[:-2]})")
                 self.connect.commit()
 
                 termins = cursor.fetchall()
                 for termin0 in termins:
+                    #Проверяем термины на невоспроизводимые символы
                     termin = str(termin0[0]).split('/')[-1].replace('_', ' ').encode('utf-16').decode('utf-16').replace('#', ' ')
                     if '%' not in termin:
                         k+=1
@@ -89,6 +93,7 @@ class wikiDB:
         cursor.close()
         return ans, k
 
+    # Берём все ссылки которые не проверели
     def get_wiki_word(self):
         with self.connect.cursor() as cursor:
             cursor.execute("SELECT id,url,power FROM wikiurl where status != 'pic' ORDER BY power DESC ")
@@ -98,6 +103,7 @@ class wikiDB:
 
         return row
 
+    #Устанавливаем на определенную ссылку что мы её проверили
     def db_set_pic(self, id):
         try:
             with self.connect.cursor() as cursor:
@@ -108,6 +114,7 @@ class wikiDB:
         finally:
             cursor.close()
 
+    # Устанавливаем на все ссылки что мы их не проверели
     def db_clear_pic(self):
         with self.connect.cursor() as cursor:
             cursor.execute("UPDATE wikiurl SET status = ' '")
--- generatorWord.py
+++ generatorWord.py
@@ -8,8 +8,8 @@ import random
 from wordcloud import WordCloud, STOPWORDS
 from Wikidb import wikiDB
 
-
-def generate_coralmap(id):
+#Функция по генерации стиля
+def generate_coralmap():
     r = random.randint(0, 4)
 
     background_color = ['white', '#000000', '#2A0A29', '#2A0A0A', '#0A0A2A']
@@ -43,7 +43,7 @@ def generate_coralmap(id):
 global k
 k = 0
 
-
+#Функция по генерации json
 def generatedJson(id, name, power, Sattelites, style, sattelites):
     Sattelite = []
     for i in range(len(sattelites)):
@@ -70,10 +70,11 @@ def generatedJson(id, name, power, Sattelites, style, sattelites):
         },
         *Sattelite
     ]}
-    with open(f'mnt/HC_Volume_22862748/Json3/{int(k/10000)+1}/{id}.json', 'w') as outfile:
+    with open(f'mnt/HC_Volume_22862748/Json3/{int(k / 10000) + 1}/{id}.json', 'w') as outfile:
         json.dump(my_list, outfile, indent=4)
 
 
+# Функция по созданию картинок
 def generate_cloud(id, name, power):
     global k
     db = wikiDB()
@@ -82,41 +83,46 @@ def generate_cloud(id, name, power):
         a = {s: 0.55}
         l, p = db.get_word_from_url(name)
         text = a | l
+        # Проверяем больше ли 15 слов
         if p > 15:
             k += 1
             stopwords = set(STOPWORDS)
-            background_color, color, font = generate_coralmap(id)
+            # получаем все стили для картинки
+            background_color, color, font = generate_coralmap()
             try:
-                os.mkdir(f"mnt/HC_Volume_22862748/Img3/{int(k/10000)+1}")
+                os.mkdir(f"mnt/HC_Volume_22862748/Img3/{int(k / 10000) + 1}")
             except:
                 pass
             try:
-                os.mkdir(f"mnt/HC_Volume_22862748/Json3/{int(k/10000)+1}")
+                os.mkdir(f"mnt/HC_Volume_22862748/Json3/{int(k / 10000) + 1}")
             except:
                 pass
+            # генерируем картинку
             if font != ' ':
                 wordcloud = WordCloud(stopwords=stopwords, width=1600, height=1600, max_font_size=450, min_font_size=30,
                                       max_words=1000000, colormap=color, background_color=background_color,
                                       font_path='fonts/' + font).fit_words(text)
-                wordcloud.to_file(f'mnt/HC_Volume_22862748/Img3/{int(k/10000)+1}/{id}.png')
                 style = font
-                generatedJson(id, s, power, p, style, list(l))
+
             else:
                 wordcloud = WordCloud(stopwords=stopwords, width=1600, height=1600, max_font_size=450, min_font_size=30,
                                       max_words=1000000, colormap=color, background_color=background_color,
                                       ).fit_words(text)
-                wordcloud.to_file(f'mnt/HC_Volume_22862748/Img3/{int(k/10000)+1}/{id}.png')
-                style = font
-                generatedJson(id, s, power, p, 'Def', list(l))
+                style = 'Def'
+            # сохраняем картинку
+            wordcloud.to_file(f'mnt/HC_Volume_22862748/Img3/{int(k / 10000) + 1}/{id}.png')
+            # Запускаем генерацию json
+            generatedJson(id, s, power, p, style, list(l))
     db.db_set_pic(id)
 
 
 if __name__ == '__main__':
 
     fonts = []
-
+    # Берём все шрифты
     for filename in os.listdir("fonts"):
         fonts.append(filename)
+    # Создаём или очишаем все зависимые папки
     try:
         shutil.rmtree('mnt/HC_Volume_22862748/Img3')
         os.mkdir('mnt/HC_Volume_22862748/Img3')
@@ -131,17 +137,20 @@ if __name__ == '__main__':
     db.db_clear_pic()
     row = db.get_wiki_word()
 
-
     while True:
         print(k)
         rows = row[0:8]
         row = row[8:]
         t = []
+        # создаём потоки
         for row0 in rows:
+            # В потоки устанавлевам функцию для генерации картинок
             t.append(threading.Thread(target=generate_cloud, args=(row0[0], row0[1], row0[2])))
 
+        # Запускаем потоки
         for i in t:
             i.start()
 
+        # Ждем когда потоки перестанут работать
         for i in t:
             i.join()
